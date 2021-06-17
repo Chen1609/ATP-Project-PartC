77a50804ce7abeb1c34042a341b212a29483f3a6
Chen Kolely <kolelyh@gmail.com>
2021-06-17 17:58:31 +0300

    fixed the drag

---
+++ src/main/View/Main.java
@@ -0,0 +1,63 @@
+package View;
+
+import Model.IModel;
+import Model.MyModel;
+import main.ViewModel.MyViewModel;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+import static com.sun.javafx.fxml.expression.Expression.add;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("MyView.fxml"));
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("Super Mazes");
+        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.setMaximized(true);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+        playSound(new File("resources/music/mazeMusic.wav"));
+
+
+
+        IModel model = new MyModel();
+        MyViewModel viewModel = new MyViewModel(model);
+        View.MyViewController view = fxmlLoader.getController();
+        view.setViewModel(viewModel);
+        view.greet();
+    }
+
+    public static synchronized void playSound(final File url) {
+        new Thread(new Runnable() {
+            // The wrapper thread is unnecessary, unless it blocks on the
+            // Clip finishing; see comments.
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(url);
+                    clip.open(inputStream);
+                    clip.loop(-1);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/ViewModel/MyViewModel.java
@@ -0,0 +1,88 @@
+package main.ViewModel;
+
+import Model.IModel;
+import Model.MovementDirection;
+import algorithms.mazeGenerators.Position;
+import algorithms.search.Solution;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MyViewModel extends Observable implements Observer {
+    private IModel model;
+
+    public MyViewModel(IModel model) {
+        this.model = model;
+        this.model.assignObserver(this); //Observe the Model for it's changes
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        setChanged();
+        notifyObservers(arg);
+    }
+
+    public int[][] getMaze(){
+        return model.getMaze().getMap();
+    }
+
+    public Position getGoal() {
+        return model.getMaze().getGoalPosition();
+    }
+
+    public int getPlayerRow(){
+        return model.getPlayerRow();
+    }
+
+    public int getPlayerCol(){
+        return model.getPlayerCol();
+    }
+
+    public Solution getSolution(){
+        return model.getSolution();
+    }
+
+    public void generateMaze(int rows, int cols){
+        model.generateMaze(rows, cols);
+    }
+
+    public void movePlayer(KeyEvent keyEvent){
+        MovementDirection direction;
+        switch (keyEvent.getCode()){
+            case UP, NUMPAD8 -> direction = MovementDirection.UP;
+            case DOWN, NUMPAD2 -> direction = MovementDirection.DOWN;
+            case LEFT, NUMPAD4 -> direction = MovementDirection.LEFT;
+            case RIGHT, NUMPAD6 -> direction = MovementDirection.RIGHT;
+            case NUMPAD7 -> direction = MovementDirection.UPLeft;
+            case NUMPAD9 -> direction = MovementDirection.UPRight;
+            case NUMPAD1 -> direction = MovementDirection.DownLeft;
+            case NUMPAD3 -> direction = MovementDirection.DownRight;
+
+            default -> {
+                // no need to move the player...
+                return;
+            }
+        }
+        model.updatePlayerLocation(direction);
+    }
+
+    public void solveMaze() throws Exception {
+        model.solveMaze();
+    }
+
+    public void movePlayerWithMouse(MouseEvent mouseEvent) {
+        model.movePlayer(mouseEvent.getSceneX(), mouseEvent.getSceneY());
+    }
+
+
+    public void setProperties(String p) {
+        model.setProp(p);
+    }
+
+    public void setPropertiesGenerate(String s) {
+        model.setPropGenerate(s);
+    }
+}
